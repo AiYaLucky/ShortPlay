bb106e495f001c3c9edbdc9711c400b5c5ae046b
xuxiaowei <aiyalucky@126.com>
2023-02-21 13:55:03 +0800

    初版代码提交

---
+++ app/src/main/java/com/aiyalucky/shortplay/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.aiyalucky.shortplay.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aiyalucky.shortplay.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
