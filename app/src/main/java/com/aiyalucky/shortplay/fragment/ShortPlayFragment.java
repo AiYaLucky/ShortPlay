2406bd99d42931497562fae3e4aa5a57a0167fe4
xuxiaowei <aiyalucky@126.com>
2023-03-02 14:56:56 +0800

    代码提奥

--- app/src/main/java/com/aiyalucky/shortplay/fragment/ShortPlayFragment.java
+++ app/src/main/java/com/aiyalucky/shortplay/fragment/ShortPlayFragment.java
@@ -4,50 +4,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.aiyalucky.shortplay.R;
-import com.aiyalucky.shortplay.adapter.ImageAdapter;
+import com.aiyalucky.shortplay.adapter.CategoryAdapter;
+import com.aiyalucky.shortplay.pojo.ItemData;
+import com.aiyalucky.shortplay.pojo.MyData;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class ShortPlayFragment extends Fragment {
 
-    private ShortPlayViewModel mViewModel;
-    private GridView mGridView;
-    private List<String> mImageList;
-    private List<String> mTexts;
-    private ImageAdapter mAdapter;
-
-    public static ShortPlayFragment newInstance() {
-        return new ShortPlayFragment();
-    }
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+    private CategoryAdapter adapter;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_short_play, container, false);
-        HashMap<String, List<String>> itemData = this.getItemData();
-        mGridView = view.findViewById(R.id.grid_view);
-        mGridView.getLayoutParams().height = getResources().getDisplayMetrics().heightPixels / 2;
-        ImageAdapter adapter = new ImageAdapter(getActivity(), itemData.get("imageList"), itemData.get("texts"));
-        mGridView.setAdapter(adapter);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.short_play_fragment, container, false);
+
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        adapter = new CategoryAdapter(getContext(), createData());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            adapter.setData(createData());
+            adapter.notifyDataSetChanged();
+            swipeRefreshLayout.setRefreshing(false);
+        });
 
         return view;
     }
 
-    /**
-     * 初始化 mImages 和 mTexts 数据【后续这里有服务器回来的数据内容，直接用服务器的数据内容即可】
-     * @return 返回一个hashmap 里面imageList是图片信息  texts是对应文本信息
-     */
-    public HashMap<String, List<String>> getItemData() {
-        HashMap<String, List<String>> resultMap = new HashMap<>(16);
+    private ArrayList<MyData> createData() {
+        ArrayList<MyData> dataList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            MyData data = new MyData();
+            data.setTitle("类别 " + (i + 1));
+            List<ItemData> itemList = new ArrayList<>();
+            for (int j = 0; j < 6; j++) {
+                ItemData item = new ItemData();
+                item.setImageRes(getRandomImageResId());
+                item.setText("说明： " + (j + 1));
+                itemList.add(item);
+            }
+            data.setItems(itemList);
+            dataList.add(data);
+        }
+        return dataList;
+    }
+
+    private String getRandomImageResId() {
         List<String> imageList = new ArrayList<>();
         imageList.add("https://img1.baidu.com/it/u=953680778,1162160249&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
         imageList.add("https://img1.baidu.com/it/u=2720349980,3743060535&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
@@ -60,27 +75,9 @@ public class ShortPlayFragment extends Fragment {
         imageList.add("https://img1.baidu.com/it/u=2028618468,66183889&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
         imageList.add("https://img1.baidu.com/it/u=953680778,1162160249&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
         imageList.add("https://img1.baidu.com/it/u=2720349980,3743060535&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
-        imageList.add("https://img1.baidu.com/it/u=2028618468,66183889&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200");
-        resultMap.put("imageList", imageList);
+        Random random = new Random();
+        int index = random.nextInt(imageList.size());
 
-        List<String> texts = new ArrayList<>();
-        texts.add("Image 1");
-        texts.add("Image 2");
-        texts.add("Image 3");
-        texts.add("Image 4");
-        texts.add("Image 5");
-        texts.add("Image 6");
-        texts.add("Image 7");
-        texts.add("Image 8");
-        texts.add("Image 9");
-        resultMap.put("texts", texts);
-        return resultMap;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ShortPlayViewModel.class);
-        // TODO: Use the ViewModel
+        return imageList.get(index);
     }
 }
---
+++ app/src/main/java/com/aiyalucky/shortplay/pojo/ItemData.java
@@ -0,0 +1,36 @@
+package com.aiyalucky.shortplay.pojo;
+
+/**
+ * @Author: xu xiao wei
+ * @Date: 2023/3/1 22:54
+ * @Version 1.0
+ * Created by IntelliJ IDEA.
+ */
+public class ItemData {
+    private String imageRes;
+    private String text;
+
+    public ItemData() {
+    }
+
+    public ItemData(String imageRes, String text) {
+        this.imageRes = imageRes;
+        this.text = text;
+    }
+
+    public String getImageRes() {
+        return imageRes;
+    }
+
+    public void setImageRes(String imageRes) {
+        this.imageRes = imageRes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
